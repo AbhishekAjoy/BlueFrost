e905586eb55efd3ce5d11e701765f74d0c34946f
AbhishekAjoy <abhiajoy23@gmail.com>
2023-06-17 01:28:34 +0530

    Refactored gotLocation to service

--- src/app/_services/weatherapi.service.ts
+++ src/app/_services/weatherapi.service.ts
@@ -1,11 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'environment';
+import { BehaviorSubject } from 'rxjs';
+import { Weather } from '../_interfaces/weather.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherapiService {
+
+  weatherForecast = new BehaviorSubject<Partial<Weather>>({});
 
   constructor(private http: HttpClient) { }
 
@@ -16,7 +20,24 @@ export class WeatherapiService {
     return this.http.get(this.BASE_URL + '&q=' + city + '&aqi=yes');
   }
 
+  getLocation(){
+    if (!navigator.geolocation) {
+      console.log('Geolocation is not supported by your browser');
+    } else {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          localStorage.setItem('latitude', position.coords.latitude.toString());
+          localStorage.setItem('longitude',position.coords.longitude.toString());
+        },
+        () => {
+          console.log('unable to retreive your position! Please Refresh Page');
+        },
+        { timeout: 10000 }
+      );
+    }
+  }
   getWeatherByCurrentLocation(){
+    this.getLocation();
     let lat:string = localStorage.getItem('latitude')??'error';
     let long:string = localStorage.getItem('longitude')??'error';
     if(lat === 'error' || long === 'error'){
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,11 @@
-import { Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { WeatherapiService } from '../_services/weatherapi.service';
-import { Current, Weather, Location, Condition} from '../_interfaces/weather.interface';
+import {
+  Current,
+  Weather,
+  Location,
+  Condition,
+} from '../_interfaces/weather.interface';
 
 @Component({
   selector: 'app-main',
@@ -10,7 +15,7 @@ import { Current, Weather, Location, Condition} from '../_interfaces/weather.int
 export class MainComponent implements OnInit {
   constructor(private weatherapiService: WeatherapiService) {}
 
-  @Output() weatherData = new EventEmitter<Partial<Current>>;
+  @Output() weatherData = new EventEmitter<Partial<Current>>();
 
   currentForecast: Partial<Weather> = {};
   currentWeather: Partial<Current> = {};
@@ -18,28 +23,15 @@ export class MainComponent implements OnInit {
   currentCondition: Partial<Condition> = {};
 
   ngOnInit(): void {
-    if (!navigator.geolocation) {
-      console.log('Geolocation is not supported by your browser');
-    } else {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          localStorage.setItem('latitude', position.coords.latitude.toString());
-          localStorage.setItem('longitude',position.coords.longitude.toString());
-        },
-        () => {
-          console.log('unable to retreive your position! Please Refresh Page');
-        },
-        { timeout: 10000 }
-      );
-    }
     this.weatherapiService.getWeatherByCurrentLocation().subscribe({
       next: (result) => {
         this.currentForecast = result;
         console.log(this.currentForecast);
-        this.currentWeather = this.currentForecast.current??{};
-        this.currentLocation = this.currentForecast.location??{};
-        this.currentCondition = this.currentWeather.condition??{};
-        this.weatherData.emit(this.currentWeather);    },
+        this.currentWeather = this.currentForecast.current ?? {};
+        this.currentLocation = this.currentForecast.location ?? {};
+        this.currentCondition = this.currentWeather.condition ?? {};
+        this.weatherData.emit(this.currentWeather);
+      },
       error: (error) => {
         console.log(error.error.message);
       },
